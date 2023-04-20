3ed24bd9de1974ac00d52e96434dfdbdacf277c2
Shadow <shamunahmed38@gmail.com>
2023-04-20 16:00:58 +0600

    Added a placeholder Footer

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
 import Image from 'next/image'
 import Head from 'next/head'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 
 export default function Home() {
   return (
-    <Head>
-      <title>E-commerce</title>
-    </Head>
+    <> 
+      <Head>
+        <title>Shadow Store</title>
+        <meta name='description' content='The best items to blend in the shadows'/>
+      </Head>
+      <Navbar/>
+      <div className='w-screen relative' style={{height: "40vh"}}>
+        <Image src="/ShadowCode.jpg" fill alt='code'/>
+      </div>
+      <Footer/>
+    </>
   )
 }
