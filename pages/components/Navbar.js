3b9ab28d92618950dc6f3c1d1b8fee652923fe81
Shadow <shamunahmed38@gmail.com>
2023-04-23 20:57:34 +0600

    Added logo to Footer

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,7 +1,12 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+      <div className='logo'>
+        <Image src="/vercel.svg" width={100} height={100}/>
+      </div>
+    </div>
   )
 }
