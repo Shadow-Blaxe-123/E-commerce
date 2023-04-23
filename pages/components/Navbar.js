61e09a7c53cbfa42ee17e2e5b6964e285603cb2c
Shadow <shamunahmed38@gmail.com>
2023-04-23 21:04:17 +0600

    Started on Navbar

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -5,7 +5,16 @@ export default function Navbar() {
   return (
     <div>
       <div className='logo'>
-        <Image src="/vercel.svg" width={100} height={100}/>
+        <Image src="/favicon.ico" width={40} height={40}/>
+      </div>
+      <div className='nav'>
+        <ul className='flex'>
+            <li>About</li>
+            <li>Tshirts</li>
+            <li>Hoodies</li>
+            <li>Mugs</li>
+            <li>Stickers</li>
+        </ul>
       </div>
     </div>
   )
