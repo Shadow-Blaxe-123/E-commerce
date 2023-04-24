5f593132b21e7600b827e758f378838702a27520
Shadow <shamunahmed38@gmail.com>
2023-04-24 20:20:10 +0600

    Modification of Nav

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -5,11 +5,11 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div className='flex flex-col justify-between items-center md:flex-row md:justify-start'>
-      <div className='logo'>
+      <div className='logo mx-4 my-2 md:my-4'>
         <Link href={'/'}><Image src="/favicon.ico" width={40} height={40} alt='logo'/></Link>
       </div>
       <div className='nav'>
-        <ul className='flex items-center'>
+        <ul className='flex items-center space-x-5 font-bold'>
             <Link href={'/'}><li>About</li></Link>
             <Link href={'/'}><li>Tshirts</li></Link>
             <Link href={'/'}><li>Hoodies</li></Link>
@@ -17,7 +17,7 @@ export default function Navbar() {
             <Link href={'/'}><li>Suits</li></Link>
         </ul>
       </div>
-      <div className='cart'>
+      <div className='cart absolute right-0 mx-5 font-bold'>
         <button>Cart</button>
       </div>
     </div>
