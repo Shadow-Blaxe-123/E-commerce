fa076b80999f2589dbd8e84781dbae22edc3434d
Shadow <shamunahmed38@gmail.com>
2023-04-27 16:04:16 +0600

    Created Tshirts page.

--- pages/tshirts.js
+++ pages/tshirts.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Tshirts = () => {
@@ -6,86 +7,103 @@ const Tshirts = () => {
       <section className="text-gray-600 body-font">
   <div className="container px-5 py-24 mx-auto">
     <div className="flex flex-wrap -m-4">
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative  rounded overflow-hidden">
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-8">
+        <Link href={'/product/tshirts'}>
+        <div className="block relative  rounded overflow-hidden">
           <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61JfsRhV+NL._AC_UL400_.jpg"/>
-        </a>
+        </div>
         <div className="mt-4 text-center md:text-left">
           <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
           <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
           <p className="mt-1">$16.00</p>
         </div>
+        </Link>
       </div>
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative  rounded overflow-hidden">
-          <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block" src="https://m.media-amazon.com/images/I/81QaUk6YldL._AC_UL400_.jpg"/>
-        </a>
-        <div className="mt-4 text-center md:text-left">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-          <p className="mt-1">$21.15</p>
-        </div>
-      </div>
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative  rounded overflow-hidden">
-          <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61qojMm3mpL._AC_UL400_.jpg"/>
-        </a>
-        <div className="mt-4 text-center md:text-left">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-          <p className="mt-1">$12.00</p>
-        </div>
-      </div>
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative  rounded overflow-hidden">
-          <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block" src="https://m.media-amazon.com/images/I/618c2lEkJbL._AC_UL400_.jpg"/>
-        </a>
-        <div className="mt-4 text-center md:text-left">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-          <p className="mt-1">$18.40</p>
-        </div>
-      </div>
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative  rounded overflow-hidden">
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-8">
+        <Link href={'/product/tshirts'}>
+        <div className="block relative  rounded overflow-hidden">
           <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61JfsRhV+NL._AC_UL400_.jpg"/>
-        </a>
+        </div>
         <div className="mt-4 text-center md:text-left">
           <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
           <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
           <p className="mt-1">$16.00</p>
         </div>
+        </Link>
       </div>
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative  rounded overflow-hidden">
-          <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block" src="https://m.media-amazon.com/images/I/81QaUk6YldL._AC_UL400_.jpg"/>
-        </a>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-8">
+        <Link href={'/product/tshirts'}>
+        <div className="block relative  rounded overflow-hidden">
+          <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61JfsRhV+NL._AC_UL400_.jpg"/>
+        </div>
         <div className="mt-4 text-center md:text-left">
           <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-          <p className="mt-1">$21.15</p>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+          <p className="mt-1">$16.00</p>
         </div>
+        </Link>
       </div>
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative  rounded overflow-hidden">
-          <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61qojMm3mpL._AC_UL400_.jpg"/>
-        </a>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-8">
+        <Link href={'/product/tshirts'}>
+        <div className="block relative  rounded overflow-hidden">
+          <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61JfsRhV+NL._AC_UL400_.jpg"/>
+        </div>
         <div className="mt-4 text-center md:text-left">
           <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-          <p className="mt-1">$12.00</p>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+          <p className="mt-1">$16.00</p>
         </div>
+        </Link>
       </div>
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative  rounded overflow-hidden">
-          <img alt="ecommerce" className="m-auto   md:m-auto md:mx-0 h-[30vh]  md:h-[36vh] block" src="https://m.media-amazon.com/images/I/618c2lEkJbL._AC_UL400_.jpg"/>
-        </a>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-8">
+        <Link href={'/product/tshirts'}>
+        <div className="block relative  rounded overflow-hidden">
+          <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61JfsRhV+NL._AC_UL400_.jpg"/>
+        </div>
         <div className="mt-4 text-center md:text-left">
           <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-          <p className="mt-1">$18.40</p>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+          <p className="mt-1">$16.00</p>
         </div>
+        </Link>
       </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-8">
+        <Link href={'/product/tshirts'}>
+        <div className="block relative  rounded overflow-hidden">
+          <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61JfsRhV+NL._AC_UL400_.jpg"/>
+        </div>
+        <div className="mt-4 text-center md:text-left">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+          <p className="mt-1">$16.00</p>
+        </div>
+        </Link>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-8">
+        <Link href={'/product/tshirts'}>
+        <div className="block relative  rounded overflow-hidden">
+          <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61JfsRhV+NL._AC_UL400_.jpg"/>
+        </div>
+        <div className="mt-4 text-center md:text-left">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+          <p className="mt-1">$16.00</p>
+        </div>
+        </Link>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-8">
+        <Link href={'/product/tshirts'}>
+        <div className="block relative  rounded overflow-hidden">
+          <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61JfsRhV+NL._AC_UL400_.jpg"/>
+        </div>
+        <div className="mt-4 text-center md:text-left">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+          <p className="mt-1">$16.00</p>
+        </div>
+        </Link>
+      </div>
+
       
      
       
