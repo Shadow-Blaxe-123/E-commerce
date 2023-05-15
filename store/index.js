ae9ade550c69e1be7566016a0e7a696fe5700f5e
Shadow <shamunahmed38@gmail.com>
2023-05-15 18:34:34 +0600

    Added Redux

--- store/index.js
+++ store/index.js
@@ -1,12 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartSlice from './cartSlice'
+import { apiSlice } from './apiSlice'
 
 
 const store = configureStore(
     {
         reducer: {
-            cart: cartSlice
-        }
+            cart: cartSlice,
+            [apiSlice.reducerPath]: apiSlice.reducer // Api Query with RTK
+        },
+
+        middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(apiSlice.middleware),
     }
 )
 
