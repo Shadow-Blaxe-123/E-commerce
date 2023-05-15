52ea8fd291c14194de9a4543da656ea285a1d767
Shadow <shamunahmed38@gmail.com>
2023-05-15 18:33:42 +0600

    Added Redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,21 @@
-import '@/styles/globals.css'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import { Provider } from 'react-redux'
-import store from '@/store'
+import "@/styles/globals.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { Provider } from "react-redux";
+import store from "@/store";
+import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
+import { apiSlice } from "@/store/apiSlice";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Provider store={store}>
-      <Navbar/>
-      <Component {...pageProps} />
-      <Footer/>
+        <ApiProvider api={apiSlice}>
+          <Navbar />
+          <Component {...pageProps} />
+          <Footer />
+        </ApiProvider>
       </Provider>
     </>
-  )
+  );
 }
