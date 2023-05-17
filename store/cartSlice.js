411d2c505972b032d5d263ce23584176bccfd541
Shadow <shamunahmed38@gmail.com>
2023-05-17 22:06:22 +0600

    Added 'AddCart' and 'ClearCar' functionality.

--- store/cartSlice.js
+++ store/cartSlice.js
@@ -1,31 +1,33 @@
 const { createSlice } = require("@reduxjs/toolkit");
 
-export const cartSlice = createSlice(
-    {
-        name: 'cart',
-        initialState: {
-            isOpen: false,
-            subTotal: 0,
-            itemsList: []
-        },
-        reducers: {
-            // State for the visibility of cart.
-            toggleCartState: (state) => {
-                state.isOpen = !state.isOpen;
-            },
-            // Adding to Cart.
-            addToCart: (state, action) => {
-                const { payload } = action;
-                const { param } = payload;
-                console.log(param);
-            }
-        }
-    }
-)
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    isOpen: false,
+    subTotal: 0,
+    itemsList: [],
+  },
+  reducers: {
+    // State for the visibility of cart.
+    toggleCartState: (state) => {
+      state.isOpen = !state.isOpen;
+    },
+    // Adding to Cart.
+    addToCart: (state, action) => {
+      //   const { payload } = action;
+      //   const { param } = payload;
+      state.itemsList.push(action.payload);
+      console.log(action.payload, state);
+    },
+    clearCart: (state) => {
+      state.itemsList = [];
+    },
+  },
+});
 
-export const {toggleCartState, addToCart} = cartSlice.actions
+export const { toggleCartState, addToCart, clearCart } = cartSlice.actions;
 // export const selectYourState = (state) => state.yourSlice.yourState;
 // export const selectYourState = (state) => state.cartSlice
-export const selectCart = (state) => state
+export const selectCart = (state) => state.cart;
 
-export default cartSlice.reducer
+export default cartSlice.reducer;
