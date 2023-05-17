eebda8ad8d0d163b7227c61ba7fd880db0a5db8b
Shadow <shamunahmed38@gmail.com>
2023-05-17 17:31:01 +0600

    Added some more to Cart state

--- store/cartSlice.js
+++ store/cartSlice.js
@@ -4,17 +4,25 @@ const cartSlice = createSlice(
     {
         name: 'cart',
         initialState: {
-            isOpen: false
+            isOpen: false,
+            subTotal: 0,
+            itemsList: []
         },
         reducers: {
             // State for the visibility of cart.
             toggleCartState: (state) => {
                 state.isOpen = !state.isOpen;
+            },
+            // Adding to Cart.
+            addToCart: (state, action) => {
+                const { payload } = action;
+                const { param } = payload;
+                console.log(param);
             }
         }
     }
 )
 
-export const {toggleCartState} = cartSlice.actions
+export const {toggleCartState, addToCart} = cartSlice.actions
 
 export default cartSlice.reducer
