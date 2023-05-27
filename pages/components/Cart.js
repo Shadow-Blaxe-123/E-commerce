21b8dad8aeb0d600b5f1775de652376af6f13a70
Shadow <shamunahmed38@gmail.com>
2023-05-27 19:42:40 +0600

    Started working on the Checkout Page.

--- pages/components/Cart.js
+++ pages/components/Cart.js
@@ -13,6 +13,7 @@ import {
   manipulateQuantity,
 } from "@/store/cartSlice";
 import { useDispatch, useSelector } from "react-redux";
+import Link from "next/link";
 
 export const Cart = () => {
   const refSideBar = useRef();
@@ -63,38 +64,47 @@ export const Cart = () => {
           className="text-purple-700 top-2 text-3xl cursor-pointer right-2 absolute"
         />
         <h2 className="font-bold text-xl flex justify-center border-b-2 border-green-500">
-          Shopping Cart Redux do something
+          Shopping Cart
         </h2>
 
         <ol className="list-decimal">
-          {cartState.itemsList.map((item) => {
-            return (
-              <li key={item.itemCode}>
-                <div className="flex m-2">
-                  <div className="w-2/3 items-center justify-center font-semibold text-lg m-2">
-                    {item.itemName}
+          {cartState.itemsList.length > 0 ? (
+            cartState.itemsList.map((item) => {
+              return (
+                <li key={item.itemCode}>
+                  <div className="flex m-2">
+                    <div className="w-2/3 items-center justify-center font-semibold text-lg m-2">
+                      {item.itemName}
+                    </div>
+                    <div className="w-1/3 flex justify-evenly items-center">
+                      <AiOutlinePlusCircle
+                        onClick={() => handleAddQuantity(item, "+")}
+                        className="text-2xl text-pink-600 cursor-pointer hover:text-pink-800"
+                      />
+                      {item.quantity}
+                      <AiOutlineMinusCircle
+                        onClick={() => handleAddQuantity(item, "-")}
+                        className="text-2xl  text-pink-600 cursor-pointer hover:text-pink-800"
+                      />
+                    </div>
                   </div>
-                  <div className="w-1/3 flex justify-evenly items-center">
-                    <AiOutlinePlusCircle
-                      onClick={() => handleAddQuantity(item, "+")}
-                      className="text-2xl text-pink-600 cursor-pointer hover:text-pink-800"
-                    />
-                    {item.quantity}
-                    <AiOutlineMinusCircle
-                      onClick={() => handleAddQuantity(item, "-")}
-                      className="text-2xl  text-pink-600 cursor-pointer hover:text-pink-800"
-                    />
-                  </div>
-                </div>
-              </li>
-            );
-          })}
+                </li>
+              );
+            })
+          ) : (
+            <div className="font-bold text-lg">No items in the Cart!!!</div>
+          )}
         </ol>
 
-        <div className="flex">
-          <button className="flex mr-auto text-white bg-pink-400 border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded">
-            <BsBagCheckFill className="my-1 mx-2" /> Checkout
-          </button>
+        <div className="font-medium">Subtotal: {cartState.subTotal}</div>
+
+        <div className="flex mt-3">
+          <Link href={"../checkout"} className="mr-auto">
+            <button className="flex mr-auto text-white bg-pink-400 border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded">
+              <BsBagCheckFill className="my-1 mx-2" /> Checkout
+            </button>
+          </Link>
+
           <button
             onClick={handleClearCart}
             className="mr-auto text-white bg-pink-400 border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded"
