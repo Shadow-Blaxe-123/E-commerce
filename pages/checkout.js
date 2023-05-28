96ebcbb08bba86c2f98a025954f947b65355cf77
Shadow <shamunahmed38@gmail.com>
2023-05-28 17:32:37 +0600

    Started on the Checkout page.

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+// import CheckoutBG from "../public/CheckoutBG.png";
 
 const Checkout = () => {
   return (
-    <div>Checkout</div>
-  )
-}
+    <div className="container bg-fixed bg-center bg-cover bg-no-repeat m-auto sm:mx-3 lg:m-10 shadow-md p-2 px-8 border-4 rounded-2xl">
+      <div className="font-black text-4xl text-center">Checkout</div>
+      <br className="bg-slate-500" />
+      <div className="details">
+        <h2 className="font-bold text-xl">1. Details</h2>
+        <div className="flex">
+          <div className="w-1/2 px-9 py-3">
+            <input type="text" />
+          </div>
+          <div className="w-1/2">
+            <input
+              type="text"
+              className="px-2 py-3 border-[3px] w-full border-rose-500"
+              placeholder="Phone"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Checkout
+export default Checkout;
--- store/index.js
+++ store/index.js
@@ -1,39 +1,35 @@
-import { configureStore } from '@reduxjs/toolkit'
-import cartSlice from './cartSlice'
-import { apiSlice } from './apiSlice'
-import { combineReducers } from 'redux';
-import { persistReducer, persistStore } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "./cartSlice";
+import { apiSlice } from "./apiSlice";
+import { combineReducers } from "redux";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-
-const rootReducer = combineReducers( {
-    cart: cartSlice,
-    [apiSlice.reducerPath]: apiSlice.reducer // Api Query with RTK
+const rootReducer = combineReducers({
+  cart: cartSlice,
+  [apiSlice.reducerPath]: apiSlice.reducer, // Api Query with RTK
 });
 
-
 const persistConfig = {
-    key: 'root',
-    storage,
-    blacklist: [`${[apiSlice.reducerPath]}`]
-}
-
+  key: "root",
+  storage,
+  blacklist: [[apiSlice.reducerPath], "register"],
+};
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+const store = configureStore({
+  reducer: persistedReducer,
 
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST"],
+      },
+    }).concat(apiSlice.middleware),
+});
 
+const persistor = persistStore(store);
 
-const store = configureStore(
-    {
-        reducer: persistedReducer,
-
-        middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(apiSlice.middleware),
-    }
-)
-
-const persistor = persistStore(store)
-
-export default store
-export {persistor}
+export default store;
+export { persistor };
