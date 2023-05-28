e3c0f935d785a1b5f2064163a81f971b8861d0ff
Shadow <shamunahmed38@gmail.com>
2023-05-28 17:29:16 +0600

    Fixed the non-serialize value error

--- pages/_app.js
+++ pages/_app.js
@@ -4,16 +4,20 @@ import Footer from "./components/Footer";
 import { Provider } from "react-redux";
 import store, { persistor } from "@/store";
 import { PersistGate } from "redux-persist/integration/react";
+import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
+// import { apiSlice } from "@/store/apiSlice";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Provider store={store}>
-        <PersistGate persistor={persistor}>
-          <Navbar />
-          <Component {...pageProps} />
-          <Footer />
-        </PersistGate>
+        {/* <ApiProvider api={apiSlice}> */}
+          <PersistGate persistor={persistor}>
+            <Navbar />
+            <Component {...pageProps} />
+            <Footer />
+          </PersistGate>
+        {/* </ApiProvider> */}
       </Provider>
     </>
   );
