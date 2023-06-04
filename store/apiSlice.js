bbd9094836a8df8eed98c1635fa50ca6d83bd8af
Shadow <shamunahmed38@gmail.com>
2023-06-04 20:11:28 +0600

    Adding Api to get the products dynamically.

--- store/apiSlice.js
+++ store/apiSlice.js
@@ -2,11 +2,20 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
+  tagTypes: ["Pincode", "Products"],
   endpoints: (builder) => ({
     checkPinCode: builder.query({
       query: () => "/pincode",
+      providesTags: () => ["Pincode"],
+    }),
+    getProdects: builder.query({
+      query: () => "/hello",
+      providesTags: () => ["Products"],
+      options: {
+        forceRefetch: true, // Add this option to always refetch the data
+      },
     }),
   }),
 });
 
-export const { useCheckPinCodeQuery } = apiSlice;
+export const { useCheckPinCodeQuery, useGetProdectsQuery } = apiSlice;
