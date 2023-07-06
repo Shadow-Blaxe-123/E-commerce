8d8faf4861bf03436302727659a28b4709e9ef56
Shadow <shamunahmed38@gmail.com>
2023-07-06 20:02:31 +0600

    Started working on order's page

---
+++ pages/order.js
@@ -0,0 +1,79 @@
+import Image from "next/image";
+import React from "react";
+
+const order = () => {
+  return (
+    <div>
+      <section class="text-gray-600 body-font overflow-hidden">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="lg:w-4/5 mx-auto flex flex-wrap">
+            <div class="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+              <h2 class="text-sm title-font text-gray-500 tracking-widest">
+                ShadowStore.com
+              </h2>
+              <h1 class="text-gray-900 text-3xl title-font font-medium mb-4">
+                Animated Night Hill Illustrations
+              </h1>
+
+              <p class="leading-relaxed mb-4">
+                Your order has been successfully placed!!!
+              </p>
+
+              <div class="flex mb-4">
+                <h2 class="font-bold flex-grow border-b-2 py-2 text-lg px-1">
+                  Item Description
+                </h2>
+                <h2 class="font-bold flex-grow border-b-2 py-2 text-lg px-1">
+                  Quantity
+                </h2>
+                <h2 class="font-bold flex-grow border-b-2 py-2 text-lg px-1">
+                  Item Total
+                </h2>
+              </div>
+
+              <div class="flex border-t border-gray-200 py-2">
+                <span class="text-gray-500">Color</span>
+                <span class="ml-auto text-gray-900">Blue</span>
+              </div>
+              <div class="flex border-t border-gray-200 py-2">
+                <span class="text-gray-500">Size</span>
+                <span class="ml-auto text-gray-900">Medium</span>
+              </div>
+              <div class="flex border-t border-b mb-6 border-gray-200 py-2">
+                <span class="text-gray-500">Quantity</span>
+                <span class="ml-auto text-gray-900">4</span>
+              </div>
+              <div class="flex">
+                <span class="title-font font-medium text-2xl text-gray-900">
+                  $58.00
+                </span>
+                <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                  Button
+                </button>
+                <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+                  <svg
+                    fill="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    class="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <Image
+              alt="ecommerce"
+              class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+              src="https://dummyimage.com/400x400"
+            />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default order;
