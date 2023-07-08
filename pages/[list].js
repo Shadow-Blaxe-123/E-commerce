1d11e9fcb24c217a2b5cd12bb1523fae88bf2da1
Shadow <shamunahmed38@gmail.com>
2023-07-08 18:38:55 +0600

    Added function to invalidate Product RTK Query cache and refetch before the [list].js page is mounted.

--- pages/[list].js
+++ pages/[list].js
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import { apiSlice, useGetProdectsQuery } from "@/store/apiSlice";
+import { apiSlice, useClearCacheMutation, useGetProdectsQuery } from "@/store/apiSlice";
 import Link from "next/link";
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
@@ -8,6 +8,9 @@ const Tshirts = () => {
   const dispatch = useDispatch();
   // dispatch(apiSlice.util.resetApiState());
   const { data: productArray = [] } = useGetProdectsQuery();
+  useEffect(() => {
+    dispatch(apiSlice.util.invalidateTags(['Products'])); // Fix the dispatch action and closing parentheses
+  }, [dispatch]);
 
   return (
     <div>
@@ -43,147 +46,6 @@ const Tshirts = () => {
             })}
 
             {console.log(productArray)}
-
-            {/* <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-8">
-              <Link href={"/product/tshirts"}>
-                <div className="block relative  rounded overflow-hidden">
-                  <img
-                    alt="ecommerce"
-                    className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block"
-                    src="https://m.media-amazon.com/images/I/61JfsRhV+NL._AC_UL400_.jpg"
-                  />
-                </div>
-                <div className="mt-4 text-center md:text-left">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                    CATEGORY
-                  </h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">
-                    The Catalyzer
-                  </h2>
-                  <p className="mt-1">$16.00</p>
-                </div>
-              </Link>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-8">
-              <Link href={"/product/tshirts"}>
-                <div className="block relative  rounded overflow-hidden">
-                  <img
-                    alt="ecommerce"
-                    className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block"
-                    src="https://m.media-amazon.com/images/I/61JfsRhV+NL._AC_UL400_.jpg"
-                  />
-                </div>
-                <div className="mt-4 text-center md:text-left">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                    CATEGORY
-                  </h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">
-                    The Catalyzer
-                  </h2>
-                  <p className="mt-1">$16.00</p>
-                </div>
-              </Link>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-8">
-              <Link href={"/product/tshirts"}>
-                <div className="block relative  rounded overflow-hidden">
-                  <img
-                    alt="ecommerce"
-                    className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block"
-                    src="https://m.media-amazon.com/images/I/61JfsRhV+NL._AC_UL400_.jpg"
-                  />
-                </div>
-                <div className="mt-4 text-center md:text-left">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                    CATEGORY
-                  </h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">
-                    The Catalyzer
-                  </h2>
-                  <p className="mt-1">$16.00</p>
-                </div>
-              </Link>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-8">
-              <Link href={"/product/tshirts"}>
-                <div className="block relative  rounded overflow-hidden">
-                  <img
-                    alt="ecommerce"
-                    className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block"
-                    src="https://m.media-amazon.com/images/I/61JfsRhV+NL._AC_UL400_.jpg"
-                  />
-                </div>
-                <div className="mt-4 text-center md:text-left">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                    CATEGORY
-                  </h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">
-                    The Catalyzer
-                  </h2>
-                  <p className="mt-1">$16.00</p>
-                </div>
-              </Link>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-8">
-              <Link href={"/product/tshirts"}>
-                <div className="block relative  rounded overflow-hidden">
-                  <img
-                    alt="ecommerce"
-                    className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block"
-                    src="https://m.media-amazon.com/images/I/61JfsRhV+NL._AC_UL400_.jpg"
-                  />
-                </div>
-                <div className="mt-4 text-center md:text-left">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                    CATEGORY
-                  </h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">
-                    The Catalyzer
-                  </h2>
-                  <p className="mt-1">$16.00</p>
-                </div>
-              </Link>
-            </div> */}
-            {/* <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-8">
-              <Link href={"/product/tshirts"}>
-                <div className="block relative  rounded overflow-hidden">
-                  <img
-                    alt="ecommerce"
-                    className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block"
-                    src="https://m.media-amazon.com/images/I/61JfsRhV+NL._AC_UL400_.jpg"
-                  />
-                </div>
-                <div className="mt-4 text-center md:text-left">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                    CATEGORY
-                  </h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">
-                    The Catalyzer
-                  </h2>
-                  <p className="mt-1">$16.00</p>
-                </div>
-              </Link>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg m-8">
-              <Link href={"/product/tshirts"}>
-                <div className="block relative  rounded overflow-hidden">
-                  <img
-                    alt="ecommerce"
-                    className="m-auto md:mx-0 h-[30vh]  md:h-[36vh] block"
-                    src="https://m.media-amazon.com/images/I/61JfsRhV+NL._AC_UL400_.jpg"
-                  />
-                </div>
-                <div className="mt-4 text-center md:text-left">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                    CATEGORY
-                  </h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">
-                    The Catalyzer
-                  </h2>
-                  <p className="mt-1">$16.00</p>
-                </div>
-              </Link>
-            </div> */}
           </div>
         </div>
       </section>
