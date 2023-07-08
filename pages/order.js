63acde034cdc137e29ad8fd892897785cc80f0e8
Shadow <shamunahmed38@gmail.com>
2023-07-08 18:40:44 +0600

    Working on Order's page front-end.

--- pages/order.js
+++ pages/order.js
@@ -35,39 +35,26 @@ const Order = () => {
               </div>
 
               {cartState.itemsList.map((item) => {
-                <div class="flex border-t border-gray-200 py-2">
-                  <span class="text-gray-500"></span>
-                  <span class="text-gray-500 ml-auto">Color</span>
-                  <span class="ml-auto text-gray-900">Blue</span>
-                </div>;
+                return (
+                  <div
+                    key={item.itemCode}
+                    class="flex border-t border-gray-200 py-2"
+                  >
+                    <span class="text-gray-500">{item.itemName}</span>
+                    <span class="text-gray-500 ml-auto">{item.quantity}</span>
+                    <span class="ml-auto text-gray-900">
+                      ${item.quantity * item.price}
+                    </span>
+                  </div>
+                );
               })}
 
-              {/* <div class="flex border-t border-gray-200 py-2">
-                <span class="text-gray-500">Size</span>
-                <span class="ml-auto text-gray-900">Medium</span>
-              </div>
-              <div class="flex border-t border-b mb-6 border-gray-200 py-2">
-                <span class="text-gray-500">Quantity</span>
-                <span class="ml-auto text-gray-900">4</span>
-              </div> */}
-              <div class="flex">
-                <span class="title-font font-medium text-2xl text-gray-900">
-                  $58.00
+              <div class="flex flex-col">
+                <span class="my-2 title-font font-medium text-2xl text-gray-900">
+                  ${cartState.subTotal}
                 </span>
-                <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                <button class="flex my-3 mr-auto text-white bg-indigo-900 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
                   Button
-                </button>
-                <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                  <svg
-                    fill="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                  </svg>
                 </button>
               </div>
             </div>
