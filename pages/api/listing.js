23fe619b7f2cffc20f79d95788a230af664a6139
Shadow <shamunahmed38@gmail.com>
2023-07-08 18:33:38 +0600

    Changed the name of hello api file name

---
+++ pages/api/listing.js
@@ -0,0 +1,55 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json([
+    {
+      itemCode: "Tshirt0009",
+      itemName: "The Catcher Eye",
+      price: 16.0,
+      img: "https://m.media-amazon.com/images/I/61JfsRhV+NL._AC_UL400_.jpg",
+      description:
+        "Introducing the perfect T-shirt for any casual occasion! Made from soft and breathable 100% cotton fabric, this T-shirt is comfortable and durable enough for everyday wear. With a classic slim-fit cut, it flatters any body type, while the range of colors allows you to choose the perfect shade to match your style. The tagless design ensures a comfortable fit, while the double-stitched seams ensure long-lasting durability. Whether you're running errands or hanging out with friends, this T-shirt is the perfect choice. It pairs effortlessly with jeans, shorts, or skirts for a comfortable and stylish look. With its high-quality fabric and attention to detail, this T-shirt is sure to become a wardrobe staple for years to come. Don't miss out on the ultimate combination of comfort and style – order your T-shirt today!",
+    },
+    {
+      itemCode: "Tshirt09990",
+      itemName: "IO",
+      price: 320,
+      img: "https://m.media-amazon.com/images/I/61JfsRhV+NL._AC_UL400_.jpg",
+      description:
+        "Introducing the perfect T-shirt for any casual occasion! Made from soft and breathable 100% cotton fabric, this T-shirt is comfortable and durable enough for everyday wear. With a classic slim-fit cut, it flatters any body type, while the range of colors allows you to choose the perfect shade to match your style. The tagless design ensures a comfortable fit, while the double-stitched seams ensure long-lasting durability. Whether you're running errands or hanging out with friends, this T-shirt is the perfect choice. It pairs effortlessly with jeans, shorts, or skirts for a comfortable and stylish look. With its high-quality fabric and attention to detail, this T-shirt is sure to become a wardrobe staple for years to come. Don't miss out on the ultimate combination of comfort and style – order your T-shirt today!",
+    },
+    {
+      itemCode: "Tshirt0980",
+      itemName: "The Catcher my Eye",
+      price: 90,
+      img: "https://m.media-amazon.com/images/I/61JfsRhV+NL._AC_UL400_.jpg",
+      description:
+        "Introducing the perfect T-shirt for any casual occasion! Made from soft and breathable 100% cotton fabric, this T-shirt is comfortable and durable enough for everyday wear. With a classic slim-fit cut, it flatters any body type, while the range of colors allows you to choose the perfect shade to match your style. The tagless design ensures a comfortable fit, while the double-stitched seams ensure long-lasting durability. Whether you're running errands or hanging out with friends, this T-shirt is the perfect choice. It pairs effortlessly with jeans, shorts, or skirts for a comfortable and stylish look. With its high-quality fabric and attention to detail, this T-shirt is sure to become a wardrobe staple for years to come. Don't miss out on the ultimate combination of comfort and style – order your T-shirt today!",
+    },
+
+    {
+      itemCode: "Tshirt90",
+      itemName: "The Ee",
+      price: 95,
+      img: "https://m.media-amazon.com/images/I/61JfsRhV+NL._AC_UL400_.jpg",
+      description:
+        "Introducing the perfect T-shirt for any casual occasion! Made from soft and breathable 100% cotton fabric, this T-shirt is comfortable and durable enough for everyday wear. With a classic slim-fit cut, it flatters any body type, while the range of colors allows you to choose the perfect shade to match your style. The tagless design ensures a comfortable fit, while the double-stitched seams ensure long-lasting durability. Whether you're running errands or hanging out with friends, this T-shirt is the perfect choice. It pairs effortlessly with jeans, shorts, or skirts for a comfortable and stylish look. With its high-quality fabric and attention to detail, this T-shirt is sure to become a wardrobe staple for years to come. Don't miss out on the ultimate combination of comfort and style – order your T-shirt today!",
+    },
+    {
+      itemCode: "Tshirt9678",
+      itemName: " Eye",
+      price: 560,
+      img: "https://m.media-amazon.com/images/I/61JfsRhV+NL._AC_UL400_.jpg",
+      description:
+        "Introducing the perfect T-shirt for any casual occasion! Made from soft and breathable 100% cotton fabric, this T-shirt is comfortable and durable enough for everyday wear. With a classic slim-fit cut, it flatters any body type, while the range of colors allows you to choose the perfect shade to match your style. The tagless design ensures a comfortable fit, while the double-stitched seams ensure long-lasting durability. Whether you're running errands or hanging out with friends, this T-shirt is the perfect choice. It pairs effortlessly with jeans, shorts, or skirts for a comfortable and stylish look. With its high-quality fabric and attention to detail, this T-shirt is sure to become a wardrobe staple for years to come. Don't miss out on the ultimate combination of comfort and style – order your T-shirt today!",
+    },
+    {
+      itemCode: "Tshirt98",
+      itemName: " Eye",
+      price: 50,
+      img: "https://m.media-amazon.com/images/I/61JfsRhV+NL._AC_UL400_.jpg",
+      description:
+        "Introducing the perfect T-shirt for any casual occasion! Made from soft and breathable 100% cotton fabric, this T-shirt is comfortable and durable enough for everyday wear. With a classic slim-fit cut, it flatters any body type, while the range of colors allows you to choose the perfect shade to match your style. The tagless design ensures a comfortable fit, while the double-stitched seams ensure long-lasting durability. Whether you're running errands or hanging out with friends, this T-shirt is the perfect choice. It pairs effortlessly with jeans, shorts, or skirts for a comfortable and stylish look. With its high-quality fabric and attention to detail, this T-shirt is sure to become a wardrobe staple for years to come. Don't miss out on the ultimate combination of comfort and style – order your T-shirt today!",
+    },
+  ]);
+}
