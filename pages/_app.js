2f5ce6815b58c8a63297a44004a9c201a543d13d
Shadow <shamunahmed38@gmail.com>
2023-07-09 15:38:31 +0600

    Added some comments.

--- pages/_app.js
+++ pages/_app.js
@@ -4,21 +4,18 @@ import Footer from "./components/Footer";
 import { Provider } from "react-redux";
 import store, { persistor } from "@/store";
 import { PersistGate } from "redux-persist/integration/react";
-import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
-// import { apiSlice } from "@/store/apiSlice";
 
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Provider store={store}>
-        {/* <ApiProvider api={apiSlice}> */}
+        {/* Using React-Persist. */}
           <PersistGate persistor={persistor}>
             <Navbar />
             <Component {...pageProps} />
             <Footer />
           </PersistGate>
-        {/* </ApiProvider> */}
       </Provider>
     </>
   );
