e3549669ee62f1c3167a856ab548bc88a47d6c79
Shadow <shamunahmed38@gmail.com>
2023-07-09 15:39:16 +0600

    Added some comments.

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
-import Image from 'next/image'
 import Head from 'next/head'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
 import { Content } from './components/Content'
 
 
 export default function Home() {
   return (
     <> 
+    {/* Meta Data */}
       <Head>
         <title>Shadow Store</title>
         <meta name='description' content='The best items to blend in the shadows'/>
